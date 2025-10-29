f430b4eb62e76ebabf850a04d5ed9de17aaa98c8
grep <grevano0813@gmail.com>
2025-10-29 12:17:53 +0700

    template web

---
+++ client/src/components/layout.js
@@ -0,0 +1,23 @@
+import Navbar from './navbar';
+import Footer from './footer';
+import Head from 'next/head';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Toko Kue Kustom</title>
+        <meta name="description" content="Jual beli kue kustom terbaik" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+      
+      <main>
+        {children}
+      </main>
+
+      <Footer />
+    </>
+  );
+}
