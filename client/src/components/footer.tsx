9ca5164c48bbb7c189e51110708bae7d7cb9b7a6
grep <grevano0813@gmail.com>
2025-11-23 12:48:09 +0700

    changing footer UI

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { FaYoutube, FaInstagram, FaWhatsapp } from 'react-icons/fa';
+import { FaYoutube, FaInstagram, FaWhatsapp, FaTiktok } from 'react-icons/fa';
 
 export default function Footer() {
   return (
@@ -24,13 +24,11 @@ export default function Footer() {
                 <FaYoutube />
               </a>
               <a href="https://www.tiktok.com/@kartiniale" target="_blank" rel="noopener noreferrer" className="social-icon" aria-label="TikTok">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-tiktok" viewBox="0 0 16 16">
-                  <path d="M9.25 0h2.44v4.25h-2.44zM6.515 4.33c-.93 0-1.783-.3-2.474-.812v6.597c0 2.37 1.46 3.362 3.823 3.362a4 4 0 0 0 2.79-1.028v-3.814a3.256 3.256 0 0 1-2.139.808c-1.286 0-2.323-.66-2.45-2.4 0 0 .009-3.52 0-3.52a3.97 3.97 0 0 0 2.462.603v-3.32z" />
-                </svg>
+                <FaTiktok/>
               </a>
-                <a href="https://api.whatsapp.com/send/?phone=6282111078050&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer" className="social-icon" aria-label="Whatsapp">
-                  <FaWhatsapp />
-                </a>
+              <a href="https://api.whatsapp.com/send/?phone=6282111078050&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer" className="social-icon" aria-label="Whatsapp">
+                <FaWhatsapp />
+              </a>
             </div>
           </div>
 
